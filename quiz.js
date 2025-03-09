cfdfdcf290f847e7379ed900f507f9ec9396e810
PrincessXeelynAArcon <princessxeelyn.arcon@smc.pshs.edu.ph>
2025-03-10 00:06:30 +0800

    quiz.js

--- quiz.js
+++ quiz.js
@@ -1,13 +1,13 @@
 document.getElementById("quiz-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form from reloading the page
+    event.preventDefault(); 
 
     let score = 0;
     let correctAnswers = {
-        q1: "B", // SDG 4 focus
-        q2: "A", // Key goal of SDG 4
-        q3: "A", // Name of project
-        q4: "C", // Featured video
-        q5: "B"  // Mission of Edu-Quality
+        q1: "B",
+        q2: "A", 
+        q3: "A",
+        q4: "C", 
+        q5: "B" 
     };
 
     for (let question in correctAnswers) {
